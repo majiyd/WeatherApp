4cc1ea8d1da815ba909511eab0fe1dd57bdfa903
Majiyd <suleimanmajiyd@gmail.com>
2019-09-04 00:42:03 +0100

    OnMount fetchs weather report using your longitude and latitude

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from 'axios'
+import * as Permissions from 'expo-permissions'
 import {
   Platform,
   StyleSheet,
@@ -25,9 +26,60 @@ export default class App extends Component {
       weather_abbr: ''
     }
   }
+  /**
+   * @type async function
+   * @desc This function asynchronously gets your location using navigator.geolocator. It is uses the longitude and latitude to fecth the weather from metaweather api but falls back to default location lagos if any error occurs
+   */
+  async getLocationAsync(){
+    const {status} = await Permissions.askAsync(Permissions.LOCATION)
+    if (status === 'granted') {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          JSON.stringify(position)
+
+          this.setState({loading: true})
+          axios.get(`https://www.metaweather.com/api/location/search/?lattlong=${position.coords.latitude},${position.coords.longitude}`)
+          .then(response => {
+            const city = response.data[0].title
+            const locationID = response.data[0].woeid
+            axios.get(`https://www.metaweather.com/api/location/${locationID}/`)
+            .then(response => {
+              const { the_temp, weather_state_abbr, weather_state_name} = response.data.consolidated_weather[0]
+              this.setState({
+                loading: false,
+                errors: null,
+                location: city,
+                temperature: the_temp,
+                weather: weather_state_name,
+                weather_abbr: weather_state_abbr
+              })
+            })
+          })
+          .catch(error => {
+            this.setState({
+              loading: false,
+              errors: error
+            })
+          })
+        },
+        error => {         
+          this.updateLocation('Lagos')
+          return
+        },
+        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+      )
+      return 
+    } else {
+      this.updateLocation('Lagos')
+      return 
+    }
+  }
   componentDidMount = () => {
-    this.updateLocation('Lagos')
-    this.setState({weather: "clear"})
+    this.getLocationAsync()
+    this.setState({
+      weather: "clear",
+      loading: true,
+    })
   }
 
   getBackgroundImage = (weather=this.state.weather_abbr) => {
